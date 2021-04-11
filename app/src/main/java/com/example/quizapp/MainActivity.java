511467c9ce7711e5a32bfa04f2ba3dd2491611a0
fahimfaisal <navid2010ps@gmail.com>
2021-04-11 12:35:20 +1000

    Addding quiz UI

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button)findViewById(R.id.button);
+        button = (Button)findViewById(R.id.startbutton);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
